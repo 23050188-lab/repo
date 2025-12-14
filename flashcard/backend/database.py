c64f27d0ec573683088e53e9a2ab953ac802eb2e
23050149-pixel <23050149@student.bdu.edu.vn>
2025-12-14 15:07:53 +0700

    Cập nhật code moi nhat

--- flashcard/backend/database.py
+++ flashcard/backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # =====================
 # DATABASE CONFIG
 # =====================
-DATABASE_URL = "postgresql://student:123456@localhost:5432/flashcard"
+DATABASE_URL = "postgresql://student:yQrceD9FL3y4q74Ks6s4fXGJYSwDPI8A@dpg-d4v6j0er433s73dvqu6g-a.virginia-postgres.render.com:5432/flashcard_82o8"
 
 # =====================
 # ENGINE
